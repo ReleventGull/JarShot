ddd5a5babed2e7b1043f3d6e137a64e2dcc3d5b7
Jaron <jaronchretien@yahoo.com>
2025-08-12 19:05:48 -0500

    feat(upgradesObject): Adds upgrade object and play button

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,5 +1,5 @@
 import { Scene } from 'phaser';
-
+import { GameState } from '../classes/upgades'
 export class MainMenu extends Scene
 {
     constructor ()
@@ -10,19 +10,30 @@ export class MainMenu extends Scene
     create ()
     {
         this.add.image(512, 384, 'background');
+        console.log(GameState)
+        GameState.calculateReloadSpeed()
 
-        this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+       this.playButton = this.add.text(512, 300, 'Play', {
+            fontFamily: 'Arial Black', fontSize: 50, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+        this.upgradeButton = this.add.text(512, 400, 'Upgrades', {
+            fontFamily: 'Arial Black', fontSize: 50, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
 
+        this.playButton.setInteractive()
+        this.upgradeButton.setInteractive()
+        this.playButton.on("pointerup", () => {
+            console.log("Starting game...")
             this.scene.start('Game');
+        })
+        this.upgradeButton.on('pointerup', () => {
+            console.log("Sending to upgrades screen...");
+        })
 
-        });
     }
 }
